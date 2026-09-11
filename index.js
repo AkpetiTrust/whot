6754e24161cdb6d71f33ad225fa4630e98c73440
Akpeti Trust <akpetitrust@gmail.com>
2026-09-11 18:09:07 +0100

    Allow wildcard CORS

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let rooms = [];
 
 const io = require("socket.io")(8080, {
   cors: {
-    origin: ["https://whot.trust-akpeti.com", "https://akpetitrust.github.io"],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
