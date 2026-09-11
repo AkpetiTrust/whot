b6fec34e0e306500f7115c6fbf2e011189d4d3bf
Akpeti Trust <akpetitrust@gmail.com>
2026-09-11 18:13:58 +0100

    Rollback to array syntax for CORS

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ let rooms = [];
 
 const io = require("socket.io")(8080, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: ["https://whot.trust-akpeti.com", "https://akpetitrust.github.io"],
   },
 });
 
